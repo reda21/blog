d7cdfc91137d2c24ff65f6370bc61ed889caeda6
reda cherfaoui <redacherfaoui@gmail.com>
2020-02-28 23:03:17 +0100

    add to heroku test

--- swagger/swagger.php
+++ swagger/swagger.php
@@ -2,7 +2,7 @@
 use OpenApi\Annotations as OA;
 
 /**
- *
+ * .\vendor\bin\openapi --format json ./public/swagger/swagger.json ./swagger/swagger.php app
  * @OA\Info(
  *     version="1.0.0",
  *     title="Webmx APi Documentation"
